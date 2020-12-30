b41fc4e524b73abf41d78465a3a12d044708cceb
John Doe <john.doe@example.com>
2020-12-30 00:07:09 -0500

    Enabling cpu-bars and memory hogs.

--- config.h
+++ config.h
@@ -15,8 +15,8 @@ static const Block blocks[] = {
 	/* {"",	"sb-price eth Ethereum 🍸",	9000,	23}, */
 	/* {"",	"sb-price btc Bitcoin 💰",				9000,	21}, */
 	{"",	"sb-torrent",	20,	7},
-	/* {"",	"sb-memory",	10,	14}, */
-	/* {"",	"sb-cpu",		10,	18}, */
+	{"",	"sb-memory",	10,	14},
+	{"",	"sb-cpubars",		10,	18},
 	/* {"",	"sb-moonphase",	18000,	17}, */
 	{"",	"sb-forecast",	18000,	5},
 	{"",	"sb-mailbox",	180,	12},
