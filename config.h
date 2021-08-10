64e3dafb85f81a66be2189cae499113bb2308720
John Doe <john.doe@example.com>
2021-08-10 11:59:59 -0400

    Adding monero to status bar and removing weather.

--- config.h
+++ config.h
@@ -7,16 +7,20 @@ static const Block blocks[] = {
 	/* {"",	"sb-music",	0,	11}, */
 	{"",	"sb-pacpackages",	0,	8},
 	{"",	"sb-news",		0,	6},
-	/* {"",	"sb-price xmr Monero 🔒 24",			9000,	24}, */
-	/* {"",	"sb-price eth Ethereum 🍸 23",	9000,	23}, */
-	/* {"",	"sb-price btc Bitcoin 💰 21",				9000,	21}, */
+	/* {"",	"sb-price lbc \"LBRY Token\" 📚",			9000,	22}, */
+	/* {"",	"sb-price bat \"Basic Attention Token\" 🦁",	9000,	20}, */
+	/* {"",	"sb-price link \"Chainlink\" 🔗",			300,	25}, */
+	{"",	"sb-price xmr \"Monero\" 🔒",			9000,	24},
+	/* {"",	"sb-price eth Ethereum 🍸",	9000,	23}, */
+	/* {"",	"sb-price btc Bitcoin 💰",				9000,	21}, */
 	{"",	"sb-torrent",	20,	7},
-	/* {"",	"sb-memory",	10,	14}, */
-	/* {"",	"sb-cpu",		10,	18}, */
+	{"",	"sb-memory",	10,	14},
+	{"",	"sb-cpu",		10,	18},
 	/* {"",	"sb-moonphase",	18000,	17}, */
-	{"",	"sb-doppler",	0,	13},
-	{"",	"sb-forecast",	18000,	5},
-	{"",	"sb-mailbox",	180,	12},
+	/* {"",	"sb-doppler",	0,	13}, */
+	/* {"",	"sb-forecast",	18000,	5}, */
+	/* {"",	"sb-forecast",	18000,	5}, */
+	/* {"",	"sb-mailbox",	180,	12}, */
 	{"",	"sb-nettraf",	1,	16},
 	{"",	"sb-volume",	0,	10},
 	{"",	"sb-battery",	5,	3},
@@ -27,9 +31,11 @@ static const Block blocks[] = {
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char *delim = " ";
+static char *delim = " -^- ";
 
 // Have dwmblocks automatically recompile and run when you edit this file in
 // vim with the following line in your vimrc/init.vim:
 
 // autocmd BufWritePost ~/.local/src/dwmblocks/config.h !cd ~/.local/src/dwmblocks/; sudo make install && { killall -q dwmblocks;setsid dwmblocks & }
+//x
+//y
